4b3412141580d2705ffc56b3861b31257c8d076e
calebdfw-wq <calebdfw@gmail.com>
2026-03-22 15:31:40 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+
 const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/AutoCart' : '',
+  assetPrefix: isGithubPages ? '/AutoCart/' : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'api.dicebear.com' },
     ],
-  },
-  experimental: {
-    // Enable server actions for future API integrations
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
   },
 }
 
